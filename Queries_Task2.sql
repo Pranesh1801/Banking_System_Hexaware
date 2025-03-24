62145f0d7ebb790087f333b1c916cc7b2b882c9f
Pranesh Rajeswaran <111297454+Pranesh1801@users.noreply.github.com>
2025-03-25 01:31:52 +0530

    Update Queries_Task2.sql

--- Queries_Task2.sql
+++ Queries_Task2.sql
@@ -4,14 +4,12 @@ INSERT INTO Customers (first_name, last_name, DOB, email, phone_number, address)
 ('Promoth', 'V', '2004-05-02', 'promoth@gmail.com', '9456789012', 'HNO-03 Main Street'),
 ('Naren', 'P', '2004-09-11', 'naren@gmail.com', '9567890123', 'HNO-04 Main Street'),
 ('Jeyasundar', 'R', '2004-04-29', 'jeyasundar@gmail.com', '9678901234', 'HNO-05 Main Street');
-
 INSERT INTO Accounts (customer_id, account_type, balance) VALUES
 (1, 'savings', 5000.00),
 (2, 'current', 40000.00),
 (3, 'savings', 20000.00),
 (4, 'current', 25000.00),
 (5, 'zero_balance', 0.00);
-
 INSERT INTO Transactions (account_id, transaction_type, amount) VALUES
 (1, 'deposit', 1000.00),
 (2, 'withdrawal', 500.00),
